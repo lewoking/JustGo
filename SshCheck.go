29cac72d55c04f94533f10f52df416c8d0cb561b
lewoking <lewoking>
2020-01-17 11:38:42 +0800

    need debug

--- SshCheck.go
+++ SshCheck.go
@@ -1,75 +1,115 @@
 package main
 
 import (
-    "fmt"
-    "github.com/mitchellh/go-homedir"
-    "golang.org/x/crypto/ssh"
-    "io/ioutil"
-    "log"
-    "time"
+	"encoding/json"
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"log"
+	"time"
 )
 
-func main(){
-    sshHost := "127.0.0.1"
-    sshUser := "liujg"
-    sshPassword := "XXXXX"
-    sshType := "password"//password 或者 key
-    sshKeyPath := ""//ssh id_rsa.id 路径"
-    sshPort := 22
+//定义配置文件解析后的结构
+type hostcfg struct {
+	sshHost     string
+	sshUser     string
+	sshPassword string
+	sshType     string
+	sshKeyPath  string
+	sshPort     int
+}
 
+type Config struct {
+	remotecmd string
+	host      hostcfg
+}
 
-    //创建sshp登陆配置
-    config := &ssh.ClientConfig{
-        Timeout:         time.Second,//ssh 连接time out 时间一秒钟, 如果ssh验证错误 会在一秒内返回
-        User:            sshUser,
-        HostKeyCallback: ssh.InsecureIgnoreHostKey(), //这个可以， 但是不够安全
-        //HostKeyCallback: hostKeyCallBackFunc(h.Host),
-    }
-    if sshType == "password" {
-        config.Auth = []ssh.AuthMethod{ssh.Password(sshPassword)}
-    } else {
-        config.Auth = []ssh.AuthMethod{publicKeyAuthFunc(sshKeyPath)}
-    }
+func main() {
+	JsonParse := NewJsonStruct()
+	v := Config{}
+	//下面使用的是相对路径，config.json文件和main.go文件处于同一目录下
+	JsonParse.Load("./config.json", &v)
+	fmt.Println(v.remotecmd)
+	fmt.Println(v.host.sshHost)
 
+	sshHost := "127.0.0.1"
+	sshUser := "liujg"
+	sshPassword := "read"
+	sshType := "password" //password 或者 key
+	sshKeyPath := ""      //ssh id_rsa.id 路径"
+	sshPort := 22
 
+	//创建sshp登陆配置
+	config := &ssh.ClientConfig{
+		Timeout:         time.Second, //ssh 连接time out 时间一秒钟, 如果ssh验证错误 会在一秒内返回
+		User:            sshUser,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), //这个可以， 但是不够安全
+		//HostKeyCallback: hostKeyCallBackFunc(h.Host),
+	}
+	if sshType == "password" {
+		config.Auth = []ssh.AuthMethod{ssh.Password(sshPassword)}
+	} else {
+		config.Auth = []ssh.AuthMethod{publicKeyAuthFunc(sshKeyPath)}
+	}
 
-    //dial 获取ssh client
-    addr := fmt.Sprintf("%s:%d", sshHost, sshPort)
-    sshClient, err := ssh.Dial("tcp", addr, config)
-    if err != nil {
-        log.Fatal("创建ssh client 失败",err)
-    }
-    defer sshClient.Close()
+	//dial 获取ssh client
+	addr := fmt.Sprintf("%s:%d", sshHost, sshPort)
+	sshClient, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		log.Fatal("创建ssh client 失败", err)
+	}
+	defer sshClient.Close()
 
-
-    //创建ssh-session
-    session, err := sshClient.NewSession()
-    if err != nil {
-        log.Fatal("创建ssh session 失败",err)
-    }
-    defer session.Close()
-    //执行远程命令
-    combo,err := session.CombinedOutput("whoami; cd /; ls -al;echo https://github.com/dejavuzhou/felix")
-    if err != nil {
-        log.Fatal("远程执行cmd 失败",err)
-    }
-    log.Println("命令输出:",string(combo))
+	//创建ssh-session
+	session, err := sshClient.NewSession()
+	if err != nil {
+		log.Fatal("创建ssh session 失败", err)
+	}
+	defer session.Close()
+	//执行远程命令
+	combo, err := session.CombinedOutput("whoami; cd /; ls -al;echo https://github.com/dejavuzhou/felix")
+	if err != nil {
+		log.Fatal("远程执行cmd 失败", err)
+	}
+	log.Println("命令输出:", string(combo))
 
 }
 
 func publicKeyAuthFunc(kPath string) ssh.AuthMethod {
-    keyPath, err := homedir.Expand(kPath)
-    if err != nil {
-        log.Fatal("find key's home dir failed", err)
-    }
-    key, err := ioutil.ReadFile(keyPath)
-    if err != nil {
-        log.Fatal("ssh key file read failed", err)
-    }
-    // Create the Signer for this private key.
-    signer, err := ssh.ParsePrivateKey(key)
-    if err != nil {
-        log.Fatal("ssh key signer failed", err)
-    }
-    return ssh.PublicKeys(signer)
+	keyPath, err := homedir.Expand(kPath)
+	if err != nil {
+		log.Fatal("find key's home dir failed", err)
+	}
+	key, err := ioutil.ReadFile(keyPath)
+	if err != nil {
+		log.Fatal("ssh key file read failed", err)
+	}
+	// Create the Signer for this private key.
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		log.Fatal("ssh key signer failed", err)
+	}
+	return ssh.PublicKeys(signer)
+}
+
+type JsonStruct struct {
+}
+
+func NewJsonStruct() *JsonStruct {
+	return &JsonStruct{}
+}
+
+func (jst *JsonStruct) Load(filename string, v interface{}) {
+	//ReadFile函数会读取文件的全部内容，并将结果以[]byte类型返回
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return
+	}
+
+	//读取的数据为json格式，需要进行解码
+	err = json.Unmarshal(data, v)
+	if err != nil {
+		return
+	}
 }
