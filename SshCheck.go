53334a1389e4ff23bdd729b41364afca4b5a7513
xerox51 <sterlingber2011@gmail.com>
2020-01-23 18:26:44 +0800

    initial

--- SshCheck.go
+++ SshCheck.go
@@ -1,89 +1,21 @@
 package main
 
 import (
+	"JustGo/config"
 	"fmt"
-	"github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
-	"golang.org/x/crypto/ssh"
-	"io/ioutil"
-	"log"
-	"time"
 )
 
-func init() {
-	viper.SetConfigName("config") //指定配置文件的文件名称(不需要制定配置文件的扩展名)
-	//viper.AddConfigPath("/etc/appname/")   //设置配置文件的搜索目录
-	//viper.AddConfigPath("$HOME/.appname")  // 设置配置文件的搜索目录
-	viper.AddConfigPath(".")    // 设置配置文件和可执行二进制文件在用一个目录
-	err := viper.ReadInConfig() // 根据以上配置读取加载配置文件
-	if err != nil {
-		log.Fatal(err) // 读取配置文件失败致命错误
-	}
-}
-
 func main() {
+	vipConfig, error := config.Init() //vipConfig是配置
+	fmt.Printf("config.init error是%v\n", error)
+	for key, val := range vipConfig.(map[string]interface{}) { //循环接口类型，获取配置信息
+		fmt.Printf("vipConfig 的key是%v val是%v\n", key, val)
 
-	fmt.Println("获取配置文件的string", viper.GetString(`remotecmd`))
-	//fmt.Println("获取配置文件的string", viper.GetInt(`host.sshPort`))
-	//fmt.Println("获取配置文件的string", viper.GetBool(`check`))
-	//fmt.Println("获取配置文件的map[string]string", viper.GetStringMapString(`host`))
-
-	sshHost := viper.GetString(`host.sshHost`)
-	sshUser := viper.GetString(`host.sshUser`)
-	sshPassword := viper.GetString(`host.sshPassword`)
-	sshType := viper.GetString(`host.sshType`)       //password 或者 key
-	sshKeyPath := viper.GetString(`host.sshKeyPath`) //ssh id_rsa.id 路径"
-	sshPort := viper.GetInt(`host.sshPort`)
-
-	//创建sshp登陆配置
-	config := &ssh.ClientConfig{
-		Timeout:         time.Second, //ssh 连接time out 时间一秒钟, 如果ssh验证错误 会在一秒内返回
-		User:            sshUser,
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(), //这个可以， 但是不够安全
-		//HostKeyCallback: hostKeyCallBackFunc(h.Host),
+		switch val.(type) { //判断val的类型
+		case map[string]interface{}: //如果是 interface接口类型
+			for ke, va := range val.(map[string]interface{}) { //循环接口类型，获取配置信息
+				fmt.Printf("vipConfig 的ke是%v va是%v\n", ke, va)
+			}
+		}
 	}
-	if sshType == "password" {
-		config.Auth = []ssh.AuthMethod{ssh.Password(sshPassword)}
-	} else {
-		config.Auth = []ssh.AuthMethod{publicKeyAuthFunc(sshKeyPath)}
-	}
-
-	//dial 获取ssh client
-	addr := fmt.Sprintf("%s:%d", sshHost, sshPort)
-	sshClient, err := ssh.Dial("tcp", addr, config)
-	if err != nil {
-		log.Fatal("创建ssh client 失败", err)
-	}
-	defer sshClient.Close()
-
-	//创建ssh-session
-	session, err := sshClient.NewSession()
-	if err != nil {
-		log.Fatal("创建ssh session 失败", err)
-	}
-	defer session.Close()
-	//执行远程命令
-	combo, err := session.CombinedOutput(viper.GetString(`remotecmd`))
-	if err != nil {
-		log.Fatal("远程执行cmd 失败", err)
-	}
-	log.Println("命令输出:", string(combo))
-
-}
-
-func publicKeyAuthFunc(kPath string) ssh.AuthMethod {
-	keyPath, err := homedir.Expand(kPath)
-	if err != nil {
-		log.Fatal("find key's home dir failed", err)
-	}
-	key, err := ioutil.ReadFile(keyPath)
-	if err != nil {
-		log.Fatal("ssh key file read failed", err)
-	}
-	// Create the Signer for this private key.
-	signer, err := ssh.ParsePrivateKey(key)
-	if err != nil {
-		log.Fatal("ssh key signer failed", err)
-	}
-	return ssh.PublicKeys(signer)
 }
